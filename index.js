9a025f04f12ce13d8f91212a4b43b3d7eba6ce69
nr072 <26773186+nr072@users.noreply.github.com>
2019-05-18 23:45:46 +0600

    removed <div> tags from inside <span> tags

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let loc = document.getElementsByClassName("page-location")[0];
-loc.querySelector("div").innerText = location.href;
+loc.innerText = location.href;
 
-let lastMod = document.querySelectorAll("span.last-modified")[0];
-lastMod.querySelector("div").innerText = document.lastModified;
+let lastMod = document.querySelectorAll("div.last-modified")[0];
+lastMod.innerText = document.lastModified;
